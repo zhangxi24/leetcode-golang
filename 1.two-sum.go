5850512c4a2dcec5784cb23befa9e7dd37ebc924
zhangxi24 <kudo894@gmail.com>
2019-10-27 18:29:48 +0800

    no. 1

---
+++ 1.two-sum.go
@@ -0,0 +1,46 @@
+/*
+ * @lc app=leetcode id=1 lang=golang
+ *
+ * [1] Two Sum
+ *
+ * https://leetcode.com/problems/two-sum/description/
+ *
+ * algorithms
+ * Easy (44.38%)
+ * Likes:    11417
+ * Dislikes: 391
+ * Total Accepted:    2M
+ * Total Submissions: 4.5M
+ * Testcase Example:  '[2,7,11,15]\n9'
+ *
+ * Given an array of integers, return indices of the two numbers such that they
+ * add up to a specific target.
+ *
+ * You may assume that each input would have exactly one solution, and you may
+ * not use the same element twice.
+ *
+ * Example:
+ *
+ *
+ * Given nums = [2, 7, 11, 15], target = 9,
+ *
+ * Because nums[0] + nums[1] = 2 + 7 = 9,
+ * return [0, 1].
+ *
+ *
+ */
+func twoSum(nums []int, target int) []int {
+	num := map[int]int{}
+	ret := []int{}
+	for i, val := range nums {
+		if _, ok := num[target-val]; ok {
+			ret = append(ret, num[target-val])
+			ret = append(ret, i)
+			return ret
+		} else {
+			num[val] = i
+		}
+
+	}
+	return ret
+}
